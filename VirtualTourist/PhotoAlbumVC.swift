5cc007e0cb318c62c2376c42b1276f7058804468
Robert Garza <robstudent1988@gmail.com>
2016-04-25 19:46:28 -0700

    PhotoAlbumVC now implements NSFetchedResultsControllerDelegate, UICollectionViewDelegate, UICollectionViewDataSource, implemented CollectionView methods

--- VirtualTourist/PhotoAlbumVC.swift
+++ VirtualTourist/PhotoAlbumVC.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 import MapKit
+import CoreData
 
-class PhotoAlbumVC: UIViewController, MKMapViewDelegate {
+private let reuseIdentifier = "photoCell"
+
+class PhotoAlbumVC: UIViewController, MKMapViewDelegate, NSFetchedResultsControllerDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var photoArray = []
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var photoCollection: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
     var latitude: Double?
     var longitude: Double?
@@ -21,8 +27,17 @@ class PhotoAlbumVC: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         
         mapView.delegate = self
+        photoCollection.delegate = self
+        photoCollection.dataSource = self
         loadPinLocation()
+        photoCollection.registerClass(PhotoCellCVC.self, forCellWithReuseIdentifier: "photoCell")
         print("\(latitude) \(longitude)")
+        
+        photoCollection.backgroundColor = UIColor.whiteColor()
+        let space: CGFloat = 3.0
+        let dimension = (self.view.frame.width - (2 * space)) / 3.0
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -32,6 +47,7 @@ class PhotoAlbumVC: UIViewController, MKMapViewDelegate {
     @IBAction func back(sender: AnyObject) {
     self.dismissViewControllerAnimated(true, completion: nil)
     }
+
     
     func loadPinLocation () {
         mapView.zoomEnabled = false
@@ -55,7 +71,43 @@ class PhotoAlbumVC: UIViewController, MKMapViewDelegate {
                 dataArray.append(item)
             }
             print(dataArray.count)
+            self.photoArray = dataArray
         }
         mapView.addAnnotation(pin)
+    }
+    
+    
+    //MARK: - Collection View
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if(section < 4) {
+            return photoArray.count
+        } else {
+            return 3
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! PhotoCellCVC
+        
+        let image = photoArray[indexPath.item] as! String
+        let imageURL = NSURL(string: image)
+        if let imageData = NSData(contentsOfURL: imageURL!) {
+            let urlData = UIImage(data: imageData)
+            let imageView = UIImageView(image: urlData)
+            
+            cell.backgroundView = imageView
+            
+        }
+        return cell
+    }
+    
+    @IBAction func refreshView(sender: AnyObject) {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.photoCollection.collectionViewLayout.invalidateLayout()
+            self.photoCollection.reloadData()
+            self.photoCollection.layoutIfNeeded()
+        })
     }
 }
---
+++ VirtualTourist/PhotoCellCVC.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCellCVC.swift
+//  VirtualTourist
+//
+//  Created by Robert Garza on 4/24/16.
+//  Copyright © 2016 Robert Garza. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCellCVC: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    func setImageView(image: UIImageView) {
+        photoImage = image
+    }
+}
